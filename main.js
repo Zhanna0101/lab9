213a0d830a34b7c1cbb32b04b61fc08d8e5d3d5d
Admin <admin@MacBook-Pro-5.local>
2022-10-25 21:42:28 +0600

    lab9

---
+++ main.js
@@ -0,0 +1,31 @@
+//2.	Write a function that finds a perimeter of the rectangle given by two sides.//
+
+function perimeter (number1, number2) { let d = 2*(number1 + number2)
+console.log(d)}
+perimeter(7,8)
+
+//14.	Write a function that returns the greatest among the given three numbers.//
+
+let x=5
+	y=7
+	z=1
+if (x>y && x>z) {console.log ("x is the greatest")
+}
+else if (y>x && y>z) {console.log ("y is the greatest")
+}
+else if (x=y && z>x ) {console.log ("z is the greatest")
+}
+else if (z=y && x>z ) {console.log ("x is the greatest")
+}
+else if (x=z && y>x ) {console.log ("y is the greatest")
+}
+else 
+{
+console.log("z is the greatest")
+}
+
+//9.	Write a function to check whether a given number is positive or negative.//
+
+let k=11
+if (k>0) {console.log ("k is positive")}
+	else { console.log("k is negative")}
